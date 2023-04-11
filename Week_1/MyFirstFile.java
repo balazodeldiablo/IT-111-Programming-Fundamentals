8e0f662189ba538d3a7cf3e9e411d74dadcd1ca4
balazodeldiablo <63554909+balazodeldiablo@users.noreply.github.com>
2023-04-11 00:05:07 -0700

    Update MyFirstFile.java

--- Week_1/MyFirstFile.java
+++ Week_1/MyFirstFile.java
@@ -1,5 +1,5 @@
 public class MyFirstFile {
     public static void main(String[] args) {
-        System.out.println("Hi, this is IT111 - Programming Fundamentals!")
+        System.out.println("Hi, this is IT111 - Programming Fundamentals!");
     }
 }
