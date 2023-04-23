d7d78e35c81bdbfa656a86db3fdb63843390bc74
balazodeldiablo <63554909+balazodeldiablo@users.noreply.github.com>
2023-04-23 15:15:32 -0700

    Create MyVariable1.java

---
+++ Week_1/MyVariable1.java
@@ -0,0 +1,23 @@
+public class MyVariable1 {
+    public static void main (String[] args) {
+        String myName = "Andy";
+        int age = 28;
+
+        System.out.println(myName);
+        System.out.println(age);
+
+        String car, guitar, tv, show;
+        car = "Honda";
+        guitar = "Yamaha";
+        tv = "Samsung";
+        show = "Rick and Morty";
+
+        int strings, chairs, chicken;
+        strings = 6;
+        chairs = 2;
+        chicken = 1;
+
+        System.out.println("My car is a "+car+". My guitar is a "+guitar+". I'm watching "+show+" in my "+tv+" TV.");
+        System.out.println("My guitar has "+strings+" strings. There are "+chairs+" chairs in the living room. If I had a pet, it would only be "+chicken+" chicken.");
+    }
+}
