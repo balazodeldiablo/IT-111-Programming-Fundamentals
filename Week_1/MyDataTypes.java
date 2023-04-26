cc955343cf37ea50f0cf8e0dd1684269b17ab80b
balazodeldiablo <63554909+balazodeldiablo@users.noreply.github.com>
2023-04-25 20:36:14 -0700

    Update MyDataTypes.java

--- Week_1/MyDataTypes.java
+++ Week_1/MyDataTypes.java
@@ -1,15 +1,18 @@
 public class MyDataTypes {
     public static void main (String[] args) {
+        
         short daysInAYear = 365;
-        int salary = 125000;
-        long minutes = 2233720368L;
-        float average = 1.8F;
+        int yearsExperience = 5;
+        double salary = 125000D;
+        long minutesPerYear = 124800L;
         double dollarsAnHour = 26.5D;
         boolean value = true;
         char myChar = 'A';
+        double salaryPerMinute =  salary / minutesPerYear;
 
-        System.out.println("Last year had "+daysInAYear+" days. I earned about "+salary+" dollars. I worked for around "+minutes+" minutes.");
-        System.out.println("Thus, I averaged around "+average+" cents per minute at "+dollarsAnHour+" an hour. I really need a raise!!");
+        System.out.println("Last year had "+daysInAYear+" days. I earned about "+salary+" dollars. I worked for around "+minutesPerYear+" minutes.");
+        System.out.println("Thus, I averaged around "+average+" dollars per minute at "+dollarsAnHour+" an hour. I really need a raise!!");
+        System.out.println("I've been working for around "+yearsExperience+" years."
 
         if (value == true) {
             System.out.println("My name is Andy. My initial is "+myChar+".");
